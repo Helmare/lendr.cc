b3427d7e20d10bd437e423c57181fc484f520199
HappiiDreamer <happiidreamer@gmail.com>
2023-02-23 04:45:08 -0600

    Created navbar with dummy logout button.

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,43 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+  import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <nav>
+    <span class="logo">lender.cc</span>
+    <button class="right">Logout</button>
+  </nav>
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<style>
+  nav {
+    background-color: black;
+    height: 5em;
+  }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+  nav a, nav span, nav button {
+    display: inline-block;
+    font-size: 2em;
+    line-height: 2.5em;
+    padding: 0em 1em;
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+    font-family: inherit;
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    color: white;
+    border: none;
+    outline: none;
+    background: none;
+    text-decoration: none;
+  }
+  nav a:hover, nav button:hover {
+    background-color: rgba(255, 255, 255, 0.5);
   }
 
   .logo {
-    margin: 0 2rem 0 0;
+    font-weight: bold;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .right {
+    float: right;
   }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
