53c320217a4b0d3acb4b305d800f0bfeef7214fe
HappiiDreamer <happiidreamer@gmail.com>
2023-02-26 16:32:50 -0600

    Added functionality to reset password view.

--- src/views/ResetPasswordView.vue
+++ src/views/ResetPasswordView.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { inject } from 'vue';
+  import { inject, ref } from 'vue';
   import { useRouter, useRoute } from 'vue-router';
   import BasicForm from '../components/BasicForm.vue';
 
@@ -7,27 +7,44 @@
   const route = useRoute();
   const resetFlag = route.query.f;
 
+  const password0 = ref('');
+  const password1 = ref('');
+
   /** @type {import('../api/lendr').default} */
   const lendr = inject('lendrClient');
 
   /**
    * Resets the password using the reset flag.
    */
-  function reset() {
-    console.log(resetFlag);
+  async function reset() {
+    if (password0.value != password1.value) return;
+    
+    const res = await lendr.post('/member/reset-password', {
+      resetFlag: resetFlag,
+      password: password0.value
+    }, { method: 'PATCH' });
+
+    if (res.status == 200) {
+      router.push('/login');
+    }
+    else {
+      // FAILED!
+    }
   }
 
   // Redirect if reset flag isn't valid.
   if (!resetFlag) {
-    router.push({ path: '/login' });
+    router.push('/login');
   }
 </script>
 
 <template>
   <BasicForm action="Reset Password" @submit.prevent="reset">
+    <div v-show="password0 != password1"></div>
+    <div v-show="password0 != password1" style="line-height: 4em; color: red">Passwords do not match.</div>
     <label>New Password</label>
-    <input id="password0" type="password" placeholder="1234" />
+    <input v-model="password0" type="password" placeholder="1234" />
     <label>Retype Password</label>
-    <input id="password1" type="password" placeholder="1234" />
+    <input v-model="password1" type="password" placeholder="1234" />
   </BasicForm>
 </template>
